38b90ebe739bb7c48b76b23ce7a4c3a7ce22fb97
LucioGhedo <lucioghedina@gmail.com>
2022-06-17 14:30:05 +0200

    bonus folder

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -68,3 +68,19 @@ for(let i = 0; i < userData.length; i++) {
     `
     cardContainer.innerHTML += classDraw
 }
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
+// BONUS:
+// Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: 
+// cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
+// Consigli del giorno:
+// Ragioniamo come sempre a step.
+// Scriviamo prima la logica in italiano e poi traduciamo in codice.
+// console.log() è nostro amico.
+// Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
+// Trovate in allegato lo zip con il layout di partenza.
+// Per la consegna dovrete creare una nuova cartella con il nome della repo e copiare dentro questa cartella i file contenuti nello zip. Poi fate un primo push e andate avanti col lavoro.
+// Vi ricordo che sono richiesti almeno 3 push al giorno.
+// Buon lavoro e buon fine settimana
+const btn = document.getElementById('addMemberButton')
+
+console.log(btn)
