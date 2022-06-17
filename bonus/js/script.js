31bb6ff2e5e3f6e366454264b5381d3a99de8dd2
LucioGhedo <lucioghedina@gmail.com>
2022-06-17 16:21:34 +0200

    fine bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,7 +1,6 @@
 //  Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e dovrà quindi essere rimossa dall’html.
 // Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 const cardContainer = document.querySelector('.team-container')
-console.log(cardContainer)
 const userData = [
     {
         img : `<img
@@ -68,19 +67,29 @@ for(let i = 0; i < userData.length; i++) {
     `
     cardContainer.innerHTML += classDraw
 }
-// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
-// BONUS:
-// Utilizzare gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: 
-// cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
-// Consigli del giorno:
-// Ragioniamo come sempre a step.
-// Scriviamo prima la logica in italiano e poi traduciamo in codice.
-// console.log() è nostro amico.
-// Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
-// Trovate in allegato lo zip con il layout di partenza.
-// Per la consegna dovrete creare una nuova cartella con il nome della repo e copiare dentro questa cartella i file contenuti nello zip. Poi fate un primo push e andate avanti col lavoro.
-// Vi ricordo che sono richiesti almeno 3 push al giorno.
-// Buon lavoro e buon fine settimana
-const btn = document.getElementById('addMemberButton')
 
-console.log(btn)
+const btnTest = document.getElementById('addMemberButton')
+
+btnTest.addEventListener('click', insertcose);
+
+function insertcose() {
+  const userName = document.querySelector('#name').value
+  const userRole = document.querySelector('#role').value
+  const img = document.querySelector('#image').value
+  
+  const classDraw = `
+    <div class="team-card">
+            <div class="card-image">
+            <img
+            src="${img}"
+            alt="${userName}"
+          />
+            </div>
+            <div class="card-text">
+              <h3>${userName}</h3>
+              <p>${userRole}</p>
+            </div>
+          </div>
+    `
+    cardContainer.innerHTML += classDraw
+}
